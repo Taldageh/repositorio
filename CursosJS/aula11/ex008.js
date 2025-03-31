667d7e535ce7f55a25f533236d0ca60dc12e45b1
Taldageh <geovanna.cristina2014@gmail.com>
2025-03-31 09:41:19 -0300

    java.script

---
+++ CursosJS/aula11/ex008.js
@@ -0,0 +1,7 @@
+//console.log('O console funcionou corretamente')
+var vel = 12
+console.log(`A velocidade do seu carrro é ${vel}km/h`)
+if(vel > 60) { //condição simples
+    console.log(`Você ultrapassou a velocidade permiida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ CursosJS/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país != 'Brasl') {
+    console.log('Brasileiro!')
+} else {
+    console.log('Estrangeiro!')
+}
