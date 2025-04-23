8523fbf2eaa3449d3a70229a1eb0d20d1d9420a6
Taldageh <geovanna.cristina2014@gmail.com>
2025-04-23 12:50:14 -0300

    skfskfskf

---
+++ copiando-projetos-02/scripts.js
@@ -0,0 +1,14 @@
+let prevButton = document.getElementById('prev')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let items = container.querySelectorAll('.list . item')
+let indicator = document.querySelectorAll('.indicators')
+let dots = indicator.querySelectorAll('ul li')
+
+prevButton.onclick = () => {
+    console.log("Botão next")
+}
+
+prevButton.onclick = () => {
+    console.log("Botão prev")
+}
