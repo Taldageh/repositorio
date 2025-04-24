fcf055a4370e7e46849bc99171bd9a727cad801d
Taldageh <geovanna.cristina2014@gmail.com>
2025-04-24 09:55:37 -0300

    dfkdpkgd

--- copiando-projetos-02/scripts.js
+++ copiando-projetos-02/scripts.js
@@ -5,8 +5,13 @@ let items = container.querySelectorAll('.list . item')
 let indicator = document.querySelectorAll('.indicators')
 let dots = indicator.querySelectorAll('ul li')
 
+let active = 0
+let firstPosition = 0
+let lastPosition = items.length - 1
+
 prevButton.onclick = () => {
-    console.log("Botão next")
+    let itemOld = container.querySelector('.List .item .active')
+    
 }
 
 prevButton.onclick = () => {
