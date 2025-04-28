a2c3ebe02c775723fa6b4c07607c0d0de7523cbd
Taldageh <geovanna.cristina2014@gmail.com>
2025-04-28 12:34:22 -0300

    oooooooooo

--- copiando-projetos-02/scripts.js
+++ copiando-projetos-02/scripts.js
@@ -1,9 +1,10 @@
 let prevButton = document.getElementById('prev')
 let nextButton = document.getElementById('next')
 let container = document.querySelector('.container')
-let items = container.querySelectorAll('.list . item')
+let items = container.querySelectorAll('.list .item')
 let indicator = document.querySelectorAll('.indicators')
 let dots = indicator.querySelectorAll('ul li')
+
 
 let active = 0
 let firstPosition = 0
