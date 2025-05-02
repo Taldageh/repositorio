2987a3c1c7fa7d720a43b7fb7deab007c772a0a8
Taldageh <geovanna.cristina2014@gmail.com>
2025-05-02 11:39:23 -0300

    9999999999999999999999999999999

--- copiando-projetos-02/scripts.js
+++ copiando-projetos-02/scripts.js
@@ -2,7 +2,7 @@ let prevButton = document.getElementById('prev')
 let nextButton = document.getElementById('next')
 let container = document.querySelector('.container')
 let items = container.querySelectorAll('.list .item')
-let indicator = document.querySelectorAll('.indicators')
+let indicator = document.querySelector('.indicators')
 let dots = indicator.querySelectorAll('ul li')
 
 
@@ -10,11 +10,29 @@ let active = 0
 let firstPosition = 0
 let lastPosition = items.length - 1
 
-prevButton.onclick = () => {
-    let itemOld = container.querySelector('.List .item .active')
-    
+nextButton.onclick = () => {
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    active = active + 1 > lastPosition ? 0 : active + 1
+    items[active].classList.add('active')
+
+    let dotsOld = indicator.querySelector('ul li.active')
+    dotsOld.classList.remove('active')
+    dots[active].classList.add('active')
+
+    indicator.querySelector('.number').innerHTML = '0' + (active + 1)
 }
 
 prevButton.onclick = () => {
-    console.log("Botão prev")
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    active = active + 1 < lastPosition ? lastPosition : active - 1
+    items[active].classList.add('active')
+
+    let dotsOld = indicator.querySelector('ul li.active')
+    dotsOld.classList.remove('active')
+    dots[active].classList.add('active')
+    indicator.querySelector('.number').innerHTML = '0' + (active + 1)
 }
