bea80c04b44738555a0651031d702c2a127adad6
Taldageh <geovanna.cristina2014@gmail.com>
2025-05-05 10:57:14 -0300

    7779646

--- copiando-projetos-05/src/javascript/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#mobile_btn').on('click', function(){
-        $('#mobile_menu').toggleclass('active');
-        $('#mobile_btn').find('i').toggleclass('fa-x');
-    });
-});
