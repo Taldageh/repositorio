c905b9fb3cd03b5f8c401dd1694062dd84868340
Geovanna Cristina dos Santos <geovanna.cristina2014@gmail.com>
2025-07-17 12:54:16 -0300

    aaaaaaaaaaa

--- copiando-projetos-05/src/javascript/script.js
+++ copiando-projetos-05/src/javascript/script.js
@@ -1,35 +1,62 @@
-$(document).ready(function() {
-    $('#mobile_btn').on('click', function(){
+$(document).ready(function () {
+    $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
     });
 
     const sections = $('section');
-    const navLinks = $('.nav-link');
+    const navItems = $('.nav_item'); // Corrigido aqui
 
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         const header = $('header');
         let activeSectionIndex = 0;
         const scrollPosition = $(window).scrollTop() - header.outerHeight();
 
-        if (scrollPosition <= 0){
+        if (scrollPosition <= 0) {
             header.css('box-shadow', 'none');
         } else {
             header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
         }
 
-        sections.each(function(i){
+        sections.each(function (i) {
             const section = $(this);
             const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop+ section.outerHeight();
+            const sectionBottom = sectionTop + section.outerHeight();
 
-            if(scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
                 activeSectionIndex = i;
-                return false; 
+                return false;
             }
-        })
+        });
 
-        navLinks.removeClass('active');
-        $(navItems[activeSectionIndex]).addclass('active');
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active'); // Corrigido aqui
+    });
+
+    // ScrollReveal - Corrigido
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#dishes .dish', {
+        origin: 'bottom',
+        duration: 1500,
+        distance: '20%',
+        interval: 200
+    });
+
+    ScrollReveal().reveal('#testimonial_chef', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%',
+        interval: 200
     });
 });
