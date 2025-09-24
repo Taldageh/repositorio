ec443998c528f311444280927b61613d0ee02d51
Geovanna Cristina dos Santos <geovanna.cristina2014@gmail.com>
2025-09-24 07:47:36 -0300

    corrige nomes de variáveis para seguir convenções de nomenclatura em men.java

--- java/Facul/men.java
+++ java/Facul/men.java
@@ -4,13 +4,13 @@ public class men
 {
 public static void main(String[] args) {
 System.out.println("A mensalidade da faculdade mensal é: 620 ");
-int facul_mensal = 620;
+int faculMensal = 620;
 
 System.out.println("A transporte da faculdade mensal é: 320 ");
-int trans_mensal = 320;
+int transMensal = 320;
 
 System.out.println("A Alimentação da faculdade mensal é: 150 ");
-int alim_mensal = 150;
+int alimMensal = 150;
 
 int facul_anual = 620 * 12;
 System.out.println("A mensalidade da faculdade anual é: " + facul_anual);
